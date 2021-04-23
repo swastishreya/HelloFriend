1b339b04bdf80fe706d83f0f3b1a53757600be3b
swastishreya <mishra.swastishreya13@gmail.com>
2021-04-24 01:40:11 +0530

    add class user attributes

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,3 +1,17 @@
 class User:
-    def __init__(self):
-        return None
+    def __init__(self, name, age, gender):
+        self._name = name
+        self._age = age
+        self._gender = gender
+
+    @property
+    def Name(self):
+        return self._name
+
+    @property
+    def Age(self):
+        return self._age
+
+    @property
+    def Gender(self):
+        return self._gender
