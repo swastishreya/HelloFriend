183b20df3b4e75b01d983167fb9d7087c8afa718
swastishreya <mishra.swastishreya13@gmail.com>
2021-04-24 01:40:42 +0530

    add code for DB transactions

--- backend/queries/connector.py
+++ backend/queries/connector.py
@@ -25,22 +25,41 @@ class Connector:
         with self._driver.session() as session:
             session.write_transaction(self._create_users, users)
 
-    def QueryUserData(self):
-        # NOTE: This a simple function that will query user data.
-        # Then It will build and return a json representation of the network
-
-        db = self.Session()
-        results = db.run("")
-        return results
-
+    def InsertLinks(self, links):
+        with self._driver.session() as session:
+            session.write_transaction(self._create_links, links)
 
     @staticmethod
     def _remove_all(tx):
         tx.run("MATCH (n) DETACH DELETE n")
 
     @staticmethod
-    def _create_users(tx, classes):
-        pass
+    def _create_users(tx, users):
+        for user in users:
+            tx.run(
+                "CREATE (u:User {name: $username, age: $user_age, gender: $user_gender})",
+                parameters={
+                    "username": user.Name,
+                    "user_age": user.Age,
+                    "user_gender": user.Gender,
+                }
+            )
+
+    @staticmethod
+    def _create_links(tx, links):
+        for link in links:
+            u1 = link[0]
+            u2 = link[1]
+            tx.run(
+                "MATCH (u1:User{name: $username1})"
+                "MATCH (u2:User{name: $username2})"
+                "MERGE (u1)-[:FRIEND_OF]->(u2)"
+                "MERGE (u1)<-[:FRIEND_OF]-(u2)",
+                parameters={
+                    "username1": u1,
+                    "username2": u2,
+                }
+            )
 
 
     def InsertTest(self):
