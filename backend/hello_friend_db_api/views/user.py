12e6f37ce89f69ef4116afca6ac6009036cdbbc5
Brahma Kulkarni <brahma.kulkarni@iiitb.org>
2021-05-16 03:34:40 +0530

    Added Login functionality

--- backend/hello_friend_db_api/views/user.py
+++ backend/hello_friend_db_api/views/user.py
@@ -55,7 +55,8 @@ def userSimilarity(request):
 
 @csrf_exempt
 def checkLogin(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
+        # print(request.body)
         json_data = json.loads(request.body)
         password = json_data['password']
         email = json_data['email']
@@ -120,9 +121,9 @@ def userDetails(request):
             for interest in interests:
                 interest_node = None
                 try:
-                    interest_node = Interest.nodes.get(name=interest.label)
+                    interest_node = Interest.nodes.get(name=interest['label'])
                 except:
-                    interest_node = Interest(name=interest.label)
+                    interest_node = Interest(name=interest['label'])
                     interest_node.save()
                 user.interestedIn.connect(interest_node)
             user.save()
@@ -163,9 +164,9 @@ def userDetails(request):
             for interest in interests:
                 interest_node = None
                 try:
-                    interest_node = Interest.nodes.get(name=interest.label)
+                    interest_node = Interest.nodes.get(name=interest['label'])
                 except:
-                    interest_node = Interest(name=interest.label)
+                    interest_node = Interest(name=interest['label'])
                     interest_node.save()
                 user.interestedIn.connect(interest_node)
             user.save()
