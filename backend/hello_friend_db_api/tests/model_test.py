e96fa2a68049b27a9b863496454a8e7058a3fa54
swastishreya <mishra.swastishreya13@gmail.com>
2021-05-15 16:42:30 +0530

    Add tests for interests node and similarity

--- backend/hello_friend_db_api/tests/model_test.py
+++ backend/hello_friend_db_api/tests/model_test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from neomodel import db, clear_neo4j_database
-from hello_friend_db_api.models import User
+from hello_friend_db_api.models import *
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 import django
@@ -11,21 +11,33 @@ class ModelTestClass(TestCase):
     def setUpTestData(cls):
         print("setUpTestData: Run once to set up non-modified data for all class methods.")
         clear_neo4j_database(db)
-        user1 = User(name='Swasti', age=22, gender='Female')
+        user1 = User(name='Swasti', age=22, gender='Female', email='swamishr@adobe.com', password='swasti123', interests=['Anime', 'Painting', 'Dancing', 'Computer Vision', 'Dogs'])
         user1.save()
-        user2 = User(name='Gurleen', age=21, gender='Female')
+        user2 = User(name='Gurleen', age=21, gender='Female', email='ginni@goldmansachs.com', password='ginni123', interests=['Painting', 'Computer Vision', 'Dogs', 'Writing'])
         user2.save()
         res = user1.friends.connect(user2)
-
-    # def setUp(self):
-    #     print("setUp: Run once for every test method to setup clean data.")
-    #     clear_neo4j_database(db)
 
     def test_save_model_user(self):
         print("Method: test_save_model_user.")      
         fetch_user1 = User.nodes.get(name='Swasti')
+        for interest in fetch_user1.interests:
+            interest_node = None
+            try:
+                interest_node = Interest.nodes.get(name=interest)
+            except:
+                interest_node = Interest(name=interest)
+                interest_node.save()
+            fetch_user1.interestedIn.connect(interest_node)
         assert fetch_user1 is not None
         fetch_user2 = User.nodes.get(name='Gurleen')
+        for interest in fetch_user2.interests:
+            interest_node = None
+            try:
+                interest_node = Interest.nodes.get(name=interest)
+            except:
+                interest_node = Interest(name=interest)
+                interest_node.save()
+            fetch_user2.interestedIn.connect(interest_node)
         assert fetch_user2 is not None
 
     def test_unique_uid(self):
@@ -38,6 +50,10 @@ class ModelTestClass(TestCase):
         fetch_user1 = User.nodes.get(name='Swasti')
         fetch_user2 = User.nodes.get(name='Gurleen')
         self.assertTrue(fetch_user1.friends.is_connected(fetch_user2))
+
+    def test_model_user_similarity(self):
+        sim, common_interest = User.getSimilarity('Swasti', 'Gurleen')
+        self.assertEqual(sim, 3)
 
     def clearTestData(self):
         clear_neo4j_database(db)
