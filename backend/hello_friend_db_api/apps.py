914056a6f288154411875dc3c917a320733d09cc
swastishreya <mishra.swastishreya13@gmail.com>
2021-05-15 16:39:58 +0530

    Add logger

--- backend/hello_friend_db_api/apps.py
+++ backend/hello_friend_db_api/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import logging
 
+logging.basicConfig(filename="logFile.txt",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 class HelloFriendDbApiConfig(AppConfig):
+    logging.info("Setting up App Config for Hello Friend Backend...")
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hello_friend_db_api'
