3a918c69561045cd0221e0031a3e8ce62d8d18d5
swastishreya <mishra.swastishreya13@gmail.com>
2021-05-16 11:08:55 +0530

    Bugs squashed

--- backend/hello_friend_db_api/views/user.py
+++ backend/hello_friend_db_api/views/user.py
@@ -39,35 +39,35 @@ def userSimilarity(request):
     if request.method == 'POST':
         json_data = json.loads(request.body)
         email = json_data['email']
-        # try:
-        user_current = User.nodes.get(email=email)
-        users = User.nodes.all()
-        response = []
-        similarity = dict()
-        logging.info("Executing Similar Users method...")
-        for user in users:
-            if user.uid != user_current.uid:
-                sim, common_interest = User.getSimilarityWithInterests(user_current.interests, user.interests)
-                obj = {
-                    "uid": user.uid,
-                    "name": user.name,
-                    "age": user.age,
-                    "gender": user.gender,
-                    "email": user.email,
-                    "interests": user.interests,
-                    "similarity_score": sim,
-                    "common_interests": list(common_interest)
-                }
-                similarity[(sim, user.uid)] = obj
-        similarity_keys = list(similarity.keys())
-        similarity_keys.sort(reverse=True)
-        for key in similarity_keys:
-            response.append(similarity[key])
-        return JsonResponse(response, safe=False)
-        # except:
-        #     response = {"error": "Error occurred"}
-        #     logging.error("Error occured while executing similar users method...")
-        #     return JsonResponse(response, safe=False)
+        try:
+            user_current = User.nodes.get(email=email)
+            users = User.nodes.all()
+            response = []
+            similarity = dict()
+            logging.info("Executing Similar Users method...")
+            for user in users:
+                if user.uid != user_current.uid:
+                    sim, common_interest = User.getSimilarityWithInterests(user_current.interests, user.interests)
+                    obj = {
+                        "uid": user.uid,
+                        "name": user.name,
+                        "age": user.age,
+                        "gender": user.gender,
+                        "email": user.email,
+                        "interests": user.interests,
+                        "similarity_score": sim,
+                        "common_interests": list(common_interest)
+                    }
+                    similarity[(sim, user.uid)] = obj
+            similarity_keys = list(similarity.keys())
+            similarity_keys.sort(reverse=True)
+            for key in similarity_keys:
+                response.append(similarity[key])
+            return JsonResponse(response, safe=False)
+        except:
+            response = {"error": "Error occurred"}
+            logging.error("Error occured while executing similar users method...")
+            return JsonResponse(response, safe=False)
 
     if request.method == 'GET':
         json_data = json.loads(request.body)
@@ -147,6 +147,7 @@ def userDetails(request):
         password = json_data['password']
         email = json_data['email']
         interests = json_data['interests']
+        interests = [interest['label'] for interest in interests]
         try:
             logging.info("Executing userDetails POST method...")
             user = User(name=name, age=age, gender=gender, password=password, email=email, interests=interests)
@@ -183,6 +184,7 @@ def userDetails(request):
         password = json_data['password']
         email = json_data['email']
         interests = json_data['interests']
+        interests = [interest['label'] for interest in interests]
         uid = json_data['uid']
         try:
             logging.info("Executing userDetails PUT method...")
